d35d8aef128c79c006531f30b12556aa525964a7
Fernando Dias <ferpaesdias@gmail.com>
2025-06-30 20:12:55 -0300

    Adição da função para instalar o DHCP Server

--- Provision/server.sh
+++ Provision/server.sh
@@ -1,26 +1,25 @@
 #!/bin/env bash
 
 source /vagrant/Provision/config_inicial.sh
+source /vagrant/Provision/dhcp_server.sh
 
 # Ajustar o fuso horário 
 echo -e "Ajustando fuso horário"
 fuso_horario
 
-
 # Ajustar os arquivos host e resolv.conf 
 echo -e "Ajustando fuso horário"
 hosts_resolv 
-
 
 # Instalar aplicativos básicos
 echo -e "Ajustando fuso horário"
 apps_basicos
 
+# Instalar o DHCP Server
+echo -e "Instalar o DHCP Server"
+instalar_dhcp_server
 
-# Instalação do DHCP Server 
-function dhcp_server {
-
-    sudo apt-get install isc-dhcp-server
-}
+# Configurar DHCP Server
+dhcp_server_interface
 
 
