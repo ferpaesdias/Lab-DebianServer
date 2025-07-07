ae59b004e8b30d6cc232a3199d4190fe39c50b74
Fernando Dias <ferpaesdias@gmail.com>
2025-07-07 17:26:22 -0300

    Configuração do DNS Server

--- DNS/dns_config.sh
+++ DNS/dns_config.sh
@@ -44,82 +44,73 @@ function bind9_check () {
 # Função que configura o DNS Server
 function bind9_config () {
 
-    sudo echo 'OPTIONS="-u bind -4"' > /etc/default/bind9
+	sudo echo 'OPTIONS="-u bind -4"' > /etc/default/bind9
 
-    cat <<-EOF > /etc/bind/named.conf.options
-    acl "trusted" {
-        172.0.0.1;      # localhost
-        172.16.0.0/24;  # Rede local
-    };
+	cat <<-EOF > /etc/bind/named.conf.options
+	options {
+	    directory "/var/cache/bind";
 
-    options {
-        directory "/var/cache/bind";
+	    recursion yes; 
+	    allow-query { 172.16.0.0/24; };
 
-        // Permite somente consultas da rede local
-        allow-query { trusted; };
+	    // Encaminhadores
+	    forwarders {
+	        8.8.8.8;
+	        8.8.4.4;
+	    }; 
 
-        // Encaminhadores
-        forwarders {
-            8.8.8.8;
-            8.8.4.4;
-        }; 
-
-        // Ativa o encaminhamento
-        forward only;
-    };
+	    dnssec-validation auto;
+	    auth-nxdomain no;
+	    listen-on { 127.0.0.1; 172.16.0.200; };
+	    listen-on-v6 { none; };
+	};
 EOF
 
-    cat <<-EOF > /etc/bind/named.conf.local  
-    // Zona para resolver nomes como 'servidor.lao.local'
-    zone "mylab.lan" {
-        type master;
-        file "/etc/bind/zones.db.mylab.lan"; // Arquivo de registro de nomes
-    };
+	cat <<-EOF > /etc/bind/named.conf.local  
+	zone "mylab.lan" {
+	    type master;
+	    file "/etc/bind/db.mylab.lan"; 
+	};
 
-    // Zonas reversa
-    zone "200.0.16.172.in-addr.arpa" {
-        type master;
-        file "/etc/bind/db.192"; //Arquivos com os registros de IPs
-    };
+	zone "0.16.172.in-addr.arpa" {
+	    type master;
+	    file "/etc/bind/db.172";
+	};
 EOF
 
-    cat <<-EOF > /etc/bind/db.mylab.lan  
-    ;
-    ; BIND data file for local loopback interface
-    ;
-    \$TTL	604800
-    @	IN	SOA	server01.mylab.lan. root.mylab.lan. (
-    			      2		; Serial
-    			 604800		; Refresh
-    			  86400		; Retry
-    			2419200		; Expire
-    			 604800 )	; Negative Cache TTL
-    ;
-    @	IN	NS	localhost.
-    
-    server	IN	A	172.16.0.200
-    firewall	IN	A	172.16.0.1
-    @	IN	A	127.0.0.1
+	cat <<-EOF > /etc/bind/db.mylab.lan  
+	;
+	; BIND data file for local loopback interface
+	;
+	\$TTL	604800
+	@	IN	SOA	server01.mylab.lan. root.mylab.lan. (
+                      2		; Serial
+                 604800		; Refresh
+                  86400		; Retry
+                2419200		; Expire
+                 604800 )	; Negative Cache TTL
+	;
+	@	IN	NS	server01.mylab.lan.
+	server01	IN	A	172.16.0.200
+	firewall	IN	A	172.16.0.1
 EOF
 
 
-    cat <<-EOF > /etc/bind/db.172 
-    ;
-    ; BIND reverse data file for local loopback interface
-    ;
-    \$TTL	604800
-    @	IN	SOA	mylab.lan. root.mylab.lan. (
-    			      1		; Serial
-    			 604800		; Refresh
-    			  86400		; Retry
-    			2419200		; Expire
-    			 604800 )	; Negative Cache TTL
-    ;
-    @	IN	NS	localhost.
-
-
-    200	IN	PTR	server01.mylab.lan.
-    1	IN	PTR	firewall.mylab.lan.
+	cat <<-EOF > /etc/bind/db.172 
+	;
+	; BIND reverse data file for local loopback interface
+	;
+	\$TTL	604800
+	@	IN	SOA	server01.mylab.lan. root.mylab.lan. (
+	                  1		; Serial
+	             604800		; Refresh
+	              86400		; Retry
+	            2419200		; Expire
+	             604800 )	; Negative Cache TTL
+	
+	@	IN	NS	server01.mylab.lan.
+	200	IN	PTR	server01.mylab.lan.
+	1	IN	PTR	firewall.mylab.lan.
 EOF
 }
 
